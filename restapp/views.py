40770ca73d86420016dcbd27688033eba23d784b
Paulo Martins <p.o.marttins@gmail.com>
2025-08-20 22:38:50 -0300

    Ajustes de retorno form e incluído messages

--- restapp/views.py
+++ restapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
+from django.contrib import messages
 from decouple import config
 import requests
 import json
 from contrib.config import obter_token
 from .forms import GrupoFormUpdate
+from this import s
 
 # Token de acesso API
 access_token = config('SECRET_KEY') # obter_token()
@@ -27,18 +29,22 @@ def lista_workspace(request):
     return render(request, 'grupos_user.html', context)
 
 def grupo_update(request):
+    form = GrupoFormUpdate(request.POST)
     if request.method == 'POST':
-        form = GrupoFormUpdate(request.POST)
+        # form = GrupoFormUpdate()
         if form.is_valid():
             email = form.cleaned_data['email']
             perfil = form.cleaned_data['perfil']
             workspace = form.cleaned_data['workspace']
 
             status, response = adiciona_user_workspace(email, perfil, workspace)
+            form = GrupoFormUpdate()
             if status == 200 or status == 201:
-                return render(request, 'grupos_user.html', {'response': response})
+                messages.success(request, 'Grupo atualizado com sucesso!')
+                return render(request, 'grupos_user.html', {'response': response, 'form': form})
             else:
-                return render(request, 'erro.html', {'response': response})
+                messages.error(request, 'Erro ao atualizar grupo!')
+                return render(request, 'grupos_user.html', {'response': response, 'form': form})
     else:
         form = GrupoFormUpdate()
 
@@ -59,6 +65,6 @@ def adiciona_user_workspace(email, perfil, workspace):
     try:
         response = requests.put(url, json=params, headers=headers)
         response.raise_for_status()
-        return response.status_code, response.json()
+        return response.status_code, response
     except requests.exceptions.RequestException as e:
         return f'Erro de requisição: {e}'
