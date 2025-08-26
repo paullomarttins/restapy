f027fa5409aff78e1cfccbccb12413a818dd0a80
Paulo Martins <p.o.marttins@gmail.com>
2025-08-26 00:17:27 -0300

    Modificado método obter_token e lista_workspace

--- contrib/config.py
+++ contrib/config.py
@@ -1,15 +1,56 @@
-# from msal import ConfidentialClientApplication
+from msal import PublicClientApplication
 from decouple import config
+import requests
+import json
 
-def obter_token():
-    client_id = config('CLIENT_ID')
-    secret_id = config('SECRET_KEY')
-    tenant_id = config('TENANT_ID')
+client_id = config('CLIENT_ID')
+secret_id = config('SECRET_KEY')
+tenant_id = config('TENANT_ID')
+username = config('USERNAME')
+password = config('PASSWORD')
 
-    authority = f'https://login.microsoftonline.com/{tenant_id}'
-    scope = ['https://analysis.windows.net/powerbi/api/.default']
+def obter_token(username, password, tenant_id, client_id, scopes):
+    app = PublicClientApplication(client_id, authority=tenant_id)
+    token_response = app.acquire_token_by_username_password(username, password, scopes)
+    return token_response['access_token']
 
-    app = ConfidentialClientApplication(client_id, authority=authority, client_crendential=secret_id)
-    token_response = app.acquire_token_for_client(scopes=scope)
+# Chama a função que gera o token para a variável access_token
+access_token = obter_token(
+    username= username,
+    password= password,
+    tenant_id=f"https://login.microsoftonline.com/{tenant_id}",
+    client_id= client_id,
+    scopes=["https://analysis.windows.net/powerbi/api/.default"],
+)
 
-    return token_response.get('access_token')
+def lista_workspace():
+    url = 'https://api.powerbi.com/v1.0/myorg/groups'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    relatorios = response.json().get('value', [])
+
+    lista = []
+    for item in relatorios:
+        lista = item['id'], item['name']
+
+    return lista
+
+def adiciona_user_workspace(email, perfil, workspace):
+    try:
+        url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace}/users'
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        }
+        params = {
+            'identifier': email,
+            'groupUserAccessRight': perfil,
+            'principalType': 'User'
+        }
+
+        response = requests.put(url, json=params, headers=headers)
+        response.raise_for_status()
+        return response.status_code, response
+    except requests.exceptions.RequestException as e:
+        return f'Erro de requisição: {e}'
