710ea96483601b7b4defd4c375d44df00b4a5bfe
Paulo Martins <p.o.marttins@gmail.com>
2025-09-02 21:14:12 -0300

    Usando models.Model GrupoFormUpdate

--- restapp/forms.py
+++ restapp/forms.py
@@ -1,21 +1,12 @@
 from django import forms
-from contrib.config import lista_workspace
+from restapp.models import GrupoUpdate
 
-WORKSPACES = lista_workspace()
-
-class GrupoFormUpdate(forms.Form):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'label': 'E-mail', 'placeholder': 'nome@exemplo.com'}))
-    perfil = forms.ChoiceField(
-        choices = [
-            ('Viewer', 'Viewer'),
-            ('Member', 'Member'),
-            ('Contributor', 'Contributor'),
-            ('Admin', 'Admin')
-        ],
-        widget=forms.Select(attrs={'class': 'form-select', 'label': 'Perfil'})
-    )
-    # workspace = forms.CharField(widget=forms.SelectMultiple(attrs={'class': 'form-select'}))
-    workspace = forms.MultipleChoiceField(
-        choices = WORKSPACES,
-        widget=forms.SelectMultiple(attrs={'class': 'form-select'})
-    )
+class GrupoFormUpdate(forms.ModelForm):
+    class Meta:
+        model = GrupoUpdate
+        fields = ['email','perfil','name']
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'nome@exemplo.com'}),
+            'perfil': forms.Select(attrs={'class': 'form-select'}),
+            'name': forms.Select(attrs={'class': 'form-select'})
+        }
