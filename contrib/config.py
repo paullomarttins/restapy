d6b2997c4d6684b0f0ffc0f34bfac3d78e102391
Paulo Martins <p.o.marttins@gmail.com>
2025-09-02 21:04:52 -0300

    Ajuste return token e salvar dados em banco

--- contrib/config.py
+++ contrib/config.py
@@ -1,18 +1,27 @@
 from msal import PublicClientApplication
 from decouple import config
 import requests
-import json
+
+from restapp.models import ListaWorkspaces
 
 client_id = config('CLIENT_ID')
-secret_id = config('SECRET_KEY')
+secret_id = config('CLIENT_SECRET')
 tenant_id = config('TENANT_ID')
 username = config('USERNAME')
 password = config('PASSWORD')
 
 def obter_token(username, password, tenant_id, client_id, scopes):
+    token_response = None
     app = PublicClientApplication(client_id, authority=tenant_id)
     token_response = app.acquire_token_by_username_password(username, password, scopes)
-    return token_response['access_token']
+
+    if 'access_token' in token_response:
+        return token_response['access_token']
+    else:
+        print('Erro ao retornar Token:')
+        for key, value in token_response.items():
+            print(f'{key}: {value}')
+        return None
 
 # Chama a função que gera o token para a variável access_token
 access_token = obter_token(
@@ -28,13 +37,17 @@ def lista_workspace():
     headers = {'Authorization': f'Bearer {access_token}'}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
-    relatorios = response.json().get('value', [])
 
-    lista = json.loads(relatorios)
-    for item in lista['name']:
-        listas = print(item['name'])
+    if response.status_code == 200:
+        relatorios = response.json().get('value', [])
 
-    return listas
+        for item in relatorios:
+            ListaWorkspaces.objects.get_or_create(
+                id_ws = item['id'],
+                type = item['type'],
+                name = item['name']
+            )
+    return response
 
 def adiciona_user_workspace(email, perfil, workspace):
     try:
