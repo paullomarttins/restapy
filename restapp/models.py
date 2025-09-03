93602ea9ebc5fee9ef24ac9e9bde74f3cbf6c57c
Paulo Martins <p.o.marttins@gmail.com>
2025-09-02 21:15:02 -0300

    Criadno models ListaWorkspaces e GrupoUpdate

--- restapp/models.py
+++ restapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+PERFIL = [
+    ('Viewer', 'Viewer'),
+    ('Member', 'Member'),
+    ('Contributor', 'Contributor'),
+    ('Admin', 'Admin')
+]
+
+class ListaWorkspaces(models.Model):
+    id_ws = models.CharField(primary_key=True, max_length=50, editable=False)
+    type = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class GrupoUpdate(models.Model):
+    email = models.EmailField()
+    perfil = models.CharField(choices=PERFIL, default='Viewer')
+    name = models.ForeignKey(ListaWorkspaces, on_delete=models.CASCADE)
+    dt_atualizacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
