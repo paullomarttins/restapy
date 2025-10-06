79ab9022a2cd6e02684eadb24252ba8c06f6051b
Paulo Martins <p.o.marttins@gmail.com>
2025-10-06 12:27:44 -0300

    Incluído loop for para adicionar workspaces ao mesmo email

--- restapp/views.py
+++ restapp/views.py
@@ -4,35 +4,37 @@ from django.contrib import messages
 
 from contrib.config import adiciona_user_workspace, lista_workspace
 from .forms import GrupoFormUpdate
-
-# Token de acesso API
-# access_token = obter_token()
+from .models import ListaWorkspaces
 
 class GrupoUserUpdate(TemplateView):
     template_name = 'grupos_user.html'
     form_class = GrupoFormUpdate
 
 def grupo_update(request):
-    lista_workspace()
-    #instance = GrupoUpdate.objects.all()
+    # lista_workspace() # incluir no crontab
+    # instance = GrupoUpdate.objects.all()
 
     if request.method == 'POST':
         form = GrupoFormUpdate(request.POST)
         if form.is_valid():
-            email = request.POST['email']
-            perfil = request.POST['perfil']
-            workspace = request.POST['name']
+            grupo_instance = form.save()
 
-            status_code, response = adiciona_user_workspace(email, perfil, workspace)
-            form.save()
-            form = GrupoFormUpdate()
+            email = form.cleaned_data['email']
+            perfil = form.cleaned_data['perfil']
+            workspaces = form.cleaned_data['name']
 
-            if status_code == 200 or status_code == 201:
+            grupo_instance.name.set(workspaces)
+
+            for workspace in workspaces:
+                status_code, response = adiciona_user_workspace(email, perfil, workspace.id_ws)
+
+            if status_code in {200, 201}:
                 messages.success(request, 'Grupo atualizado com sucesso!')
-                return render(request, 'grupos_user.html', {'response': response, 'form': form})
             else:
                 messages.error(request, 'Erro ao atualizar grupo!')
-                return render(request, 'grupos_user.html', {'response': response, 'form': form})
+
+            form = GrupoFormUpdate()
+            return render(request, 'grupos_user.html', {'response': response, 'form': form})
     else:
         form = GrupoFormUpdate()
 
