4a7eb38b35d1d3cee33d81b875b9e5cc582623fa
Paulo Martins <p.o.marttins@gmail.com>
2025-10-06 12:18:35 -0300

    Alterações método obter_token()

--- contrib/get_token.py
+++ contrib/get_token.py
@@ -1,30 +1,17 @@
-from decouple import config
+import decouple
 import msal
 
-global_token_cache = msal.TokenCache()
+config = decouple.AutoConfig('.')
 
-# Create a preferably long-lived app instance, to avoid the overhead of app creation
-global_app = msal.ConfidentialClientApplication(
-    config('CLIENT_ID'),
-    authority=config('AUTHORITY'),  # For Entra ID or External ID
-    client_credential=config('CLIENT_SECRET'),  # ENV VAR contains a JSON blob as a string
-    token_cache=global_token_cache,
-    )
-scopes = config("SCOPE", "").split()
+def obter_token(username, password, tenant_id, client_id, scopes):
+    token_response = None
+    app = msal.PublicClientApplication(client_id, authority=tenant_id)
+    token_response = app.acquire_token_by_username_password(username, password, scopes)
 
-def acquire_and_use_token():
-    result = global_app.acquire_token_for_client(scopes=scopes)
-
-    if 'access_token' in result:
-        # print(result['access_token'])
-        return result['access_token']
+    if 'access_token' in token_response:
+        return token_response['access_token']
     else:
         print('Erro ao retornar Token:')
-        for key, value in result.items():
+        for key, value in token_response.items():
             print(f'{key}: {value}')
         return None
-
-# while True:  # Here we mimic a long-lived daemon
-#     acquire_and_use_token()
-#     print("Press Ctrl-C to stop.")
-#     time.sleep(5)  # Let's say your app would run a workload every X minutes
