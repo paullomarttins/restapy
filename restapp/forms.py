d3bb4a3330b7f3fc380791eec89bd791116694b7
Paulo Martins <p.o.marttins@gmail.com>
2025-10-06 12:22:36 -0300

    Atualização GrupoFormUpdate

--- restapp/forms.py
+++ restapp/forms.py
@@ -2,11 +2,15 @@ from django import forms
 from restapp.models import GrupoUpdate
 
 class GrupoFormUpdate(forms.ModelForm):
+
     class Meta:
         model = GrupoUpdate
         fields = ['email','perfil','name']
+        labels = {
+            'name': 'Workspaces'
+        }
         widgets = {
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'nome@exemplo.com'}),
             'perfil': forms.Select(attrs={'class': 'form-select'}),
-            'name': forms.Select(attrs={'class': 'form-select'})
+            'name': forms.SelectMultiple(attrs={'class': 'form-control', 'size': 20})
         }
