2daac62791b1db85bd2142fdd0e1fbb6c849e1ae
Paulo Martins <p.o.marttins@gmail.com>
2025-10-06 12:08:24 -0300

    Atualização url api, e obter_token()

--- contrib/config.py
+++ contrib/config.py
@@ -1,35 +1,27 @@
-from msal import PublicClientApplication
-from decouple import config
+import msal
+import decouple
 import requests
 
 from restapp.models import ListaWorkspaces
+from contrib.get_token import obter_token
+
+config = decouple.AutoConfig('.')
 
 client_id = config('CLIENT_ID')
 secret_id = config('CLIENT_SECRET')
 tenant_id = config('TENANT_ID')
 username = config('USERNAME')
 password = config('PASSWORD')
-
-def obter_token(username, password, tenant_id, client_id, scopes):
-    token_response = None
-    app = PublicClientApplication(client_id, authority=tenant_id)
-    token_response = app.acquire_token_by_username_password(username, password, scopes)
-
-    if 'access_token' in token_response:
-        return token_response['access_token']
-    else:
-        print('Erro ao retornar Token:')
-        for key, value in token_response.items():
-            print(f'{key}: {value}')
-        return None
+authority = f'https://login.microsoftonline.com/{tenant_id}'
+scopes = ["https://analysis.windows.net/powerbi/api/.default"]
 
 # Chama a função que gera o token para a variável access_token
 access_token = obter_token(
     username= username,
     password= password,
-    tenant_id=f"https://login.microsoftonline.com/{tenant_id}",
+    tenant_id= authority,
     client_id= client_id,
-    scopes=["https://analysis.windows.net/powerbi/api/.default"],
+    scopes= scopes,
 )
 
 def lista_workspace():
@@ -51,7 +43,7 @@ def lista_workspace():
 
 def adiciona_user_workspace(email, perfil, workspace):
     try:
-        url = f'https://api.powerbi.com/v1.0/myorg/groups/{workspace}/users'
+        url = f'https://api.powerbi.com/v1.0/myorg/admin/groups/{workspace}/users'
         headers = {
             'Authorization': f'Bearer {access_token}',
             'Content-Type': 'application/json'
@@ -62,7 +54,7 @@ def adiciona_user_workspace(email, perfil, workspace):
             'principalType': 'User'
         }
 
-        response = requests.put(url, json=params, headers=headers)
+        response = requests.post(url, json=params, headers=headers)
         response.raise_for_status()
         return response.status_code, response
     except requests.exceptions.RequestException as e:
