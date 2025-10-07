592e3b440f23230ae29537feaa25aa194dfc1b6b
Paulo Martins <p.o.marttins@gmail.com>
2025-10-07 00:59:05 -0300

    Criado método lista_user_workspace

--- contrib/config.py
+++ contrib/config.py
@@ -2,7 +2,7 @@ import msal
 import decouple
 import requests
 
-from restapp.models import ListaWorkspaces
+from restapp.models import ListaWorkspaces, ListaUserWorkspace
 from contrib.get_token import obter_token
 
 config = decouple.AutoConfig('.')
@@ -59,3 +59,37 @@ def adiciona_user_workspace(email, perfil, workspace):
         return response.status_code, response
     except requests.exceptions.RequestException as e:
         return f'Erro de requisição: {e}'
+
+def lista_workspace_a():
+    url = 'https://api.powerbi.com/v1.0/myorg/groups'
+    headers = {'Authorization': f'Bearer {access_token}'}
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    if response.status_code == 200:
+        relatorios = response.json().get('value', [])
+
+    return relatorios
+
+def lista_user_workspace():
+    groupId = lista_workspace_a()
+    itens = []
+
+    for id_ws in groupId:
+        url = f'https://api.powerbi.com/v1.0/myorg/groups/{id_ws['id']}/users'
+        headers = {'Authorization': f'Bearer {access_token}'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        workspaces = response.json().get('value', [])
+
+        for rel in workspaces:
+            ListaUserWorkspace.objects.get_or_create(
+                id_workspace = id_ws['id'],
+                workspace_name = id_ws['name'],
+                groupuseraccess = rel['groupUserAccessRight'],
+                displayname = rel['displayName'],
+                identifier = rel['identifier'],
+                principaltype = rel['principalType']
+            )
+            itens.append(rel)
