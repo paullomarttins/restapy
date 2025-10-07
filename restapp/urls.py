fd04d7675ca918e2a8023805f2ffabc351bfa8b8
Paulo Martins <p.o.marttins@gmail.com>
2025-10-07 01:00:18 -0300

    Inclusão de novas rotas

--- restapp/urls.py
+++ restapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GrupoUserUpdate, grupo_update
+from .views import GrupoUserUpdate, grupo_update, ListaWorkspace, ListaUserWorkspaceView
 
 urlpatterns = [
     path('api/grupo/', GrupoUserUpdate.as_view(), name='view-grupo'),
-    # path('api/', lista_workspace, name='lista-workspace'),
+    path('api/workspaces/', ListaWorkspace.as_view(), name='lista-workspace'),
     path('api/grupo-update/', grupo_update, name='user-grupo-update'),
+    path('api/lista-user/', ListaUserWorkspaceView.as_view(), name='lista-user')
 ]
